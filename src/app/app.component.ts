a17712e64de08f4d35859d03fc703daa21235dd9
Julia-Marcal <anajulia.05marcal@gmail.com>
2024-06-18 21:17:32 -0300

    continuando desevolvimento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GalaxyComponent } from './galaxy/galaxy.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GalaxyComponent, NavBarComponent],
+  imports: [RouterOutlet, GalaxyComponent, NavBarComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/my-stack/my-stack.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-stack',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-stack.component.html',
+  styleUrl: './my-stack.component.css'
+})
+export class MyStackComponent {
+
+}
