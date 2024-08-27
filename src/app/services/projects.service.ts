79e7998f42db92a522e08c17c68706c756116ccf
Julia-Marcal <anajulia.05marcal@gmail.com>
2024-08-26 22:46:51 -0300

    fixing projects cards

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -10,8 +10,15 @@ export class ProjectsService {
     new Project(
       'opengenus',
       '../../assets/images/opengenus.jpg',
-      'https://br.fiverr.com/juliamarcal',
-      'Open Genus',
+      'Go Reusable Api',
+      'Go-reusable-api is a highly performant, reusable, and easy-to-understand API built with Golang, Gin, and Gorm. This API is designed to offer a robust feature set while ensuring optimized performance and security.',
+    ),
+
+    new Project(
+      'opengenus',
+      '../../assets/images/opengenus.jpg',
+      'Scraping bets and telegram-bet-bot',
+      'Revolutionize your betting experience with this innovative project, which integrates bet scraping and a Telegram bot. Using Puppeteer, it scrapes the best betting tips, games, and odds based on user-registered leagues, and delivers them directly to you via Telegram every day.',
     )
   ];
 
